5140229383d53acd3915d2feaaf0cbeb0d60d56a
Sulaiman Sudirman <sulaiman@operatebeyond.com>
2023-08-23 13:38:45 +1200

    adding more args to optimise

--- lib.js
+++ lib.js
@@ -18,7 +18,16 @@ const supportedSites = [
 // Launch the browser 
 const browser = await puppeteer.launch({
     headless: true,
-    args: ['--no-sandbox', '--disabled-setupid-sandbox']
+    args: [
+        '--no-sandbox', 
+        '--disable-setuid-sandbox',
+        '--disable-dev-shm-usage',
+        '--disable-accelerated-2d-canvas',
+        '--no-first-run',
+        '--no-zygote',
+        '--single-process',
+        '--disable-gpu'
+    ]
 });
 
 export { browser };
