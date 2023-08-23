79c8912aa5c1e3db2a5468756873b4ee6c2ff741
Sulaiman Sudirman <sulaiman@operatebeyond.com>
2023-08-23 15:49:00 +1200

    optimising more

--- lib.js
+++ lib.js
@@ -14,20 +14,52 @@ const supportedSites = [
     'www.jobstreet.com.sg',
     'www.jobstreet.com.my'
 ];
+const __dirname = new URL('.', import.meta.url).pathname;
 
 // Launch the browser 
 const browser = await puppeteer.launch({
     headless: true,
     args: [
-        '--no-sandbox', 
+        '--no-sandbox',
+        '--autoplay-policy=user-gesture-required',
+        '--disable-background-networking',
+        '--disable-background-timer-throttling',
+        '--disable-backgrounding-occluded-windows',
+        '--disable-breakpad',
+        '--disable-client-side-phishing-detection',
+        '--disable-component-update',
+        '--disable-default-apps',
+        '--disable-domain-reliability',
+        '--disable-extensions',
+        '--disable-features=AudioServiceOutOfProcess',
+        '--disable-hang-monitor',
+        '--disable-ipc-flooding-protection',
+        '--disable-notifications',
+        '--disable-offer-store-unmasked-wallet-cards',
+        '--disable-popup-blocking',
+        '--disable-print-preview',
+        '--disable-prompt-on-repost',
+        '--disable-renderer-backgrounding',
         '--disable-setuid-sandbox',
         '--disable-dev-shm-usage',
         '--disable-accelerated-2d-canvas',
+        '--disable-speech-api',
+        '--disable-sync',
+        '--hide-scrollbars',
+        '--ignore-gpu-blacklist',
+        '--metrics-recording-only',
+        '--mute-audio',
+        '--no-default-browser-check',
         '--no-first-run',
+        '--no-pings',
         '--no-zygote',
+        '--password-store=basic',
+        '--use-gl=swiftshader',
+        '--use-mock-keychain',
         '--single-process',
         '--disable-gpu'
-    ]
+    ],
+    userDataDir: __dirname + '/cache'
 });
 
 export { browser };
